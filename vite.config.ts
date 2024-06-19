f10e77ce0d2f72e0eab0e6cccf750dae7079637e
kiranmarshall <kiran.marshall@gmail.com>
2024-06-19 15:37:11 +1200

    add name to base url

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,9 @@
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
+import { name } from "./package.json";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
-  optimizeDeps: {
-    exclude: ["@ffmpeg/ffmpeg", "@ffmpeg/util"],
-  },
-  server: {
-    headers: {
-      "Cross-Origin-Opener-Policy": "same-origin",
-      "Cross-Origin-Embedder-Policy": "require-corp",
-    },
-  },
+  base: `/${name}`,
 });
